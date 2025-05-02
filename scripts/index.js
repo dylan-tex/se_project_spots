6de92dc9a5ab5411520dd0fc21dd668ac2b5a69b
Dylan Silver <dylansilver3@gmail.com>
2025-05-02 01:09:04 -0500

    test

--- scripts/index.js
+++ scripts/index.js
@@ -25,72 +25,57 @@ const initialCards = [
   },
 ];
 
-// Ties the first element with class profile__edit-btn to the the variable profileEditButton
-const profileEditButton = document.querySelector(".profile__edit-btn");
-
-// Ties the element with class profile__add-btn to the the variable newPostButton
-// when this button is hit it should trigger the opening of the addCarmFormElement (a new modal)
-const newPostButton = document.querySelector(".profile__add-btn");
-
 // Creates a Modal variable and assigns it the first element with the ID edit-profile-modal
 const editProfileModal = document.querySelector("#edit-profile-modal");
-
-// Creates the elements of the modal (close button and two inputs) and assigns them their respective values in the HTML file
-const editProfileModalCloseBtn =
-  editProfileModal.querySelector(".modal__close-btn");
 const editProfileNameInput = editProfileModal.querySelector(
   "#profile-name-input"
 );
-
-// From stage 4 part 2
-const editProfileForm = editProfileModal.querySelector(".modal__form");
-
 const editProfileDescriptionInput = editProfileModal.querySelector(
   "#profile-description-input"
 );
+const profileEditButton = document.querySelector(".profile__edit-btn");
+const editProfileModalCloseBtn =
+  editProfileModal.querySelector(".modal__close-btn");
 
-// From Stage 4 New Post Submission
 // Creates the elements of the New Post Button (close button and two inputs) and assigns them their respective values in the HTML file
 //newPostModal is a new modal
 const newPostModal = document.querySelector("#new-post-modal");
 const nameInput = newPostModal.querySelector("#new-post-link-input");
 const linkInput = newPostModal.querySelector("#new-post-caption-input");
+const newPostButton = document.querySelector(".profile__add-btn");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
-function openModal() {
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function editProfile() {
+  openModal(editProfileModal);
   editProfileNameInput.value = profileNameEl.textContent;
   editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-
-  editProfileModal.classList.add("modal_is-opened");
 }
 
-function openNewPostModal() {
-  newPostModal.classList.add("modal_is-opened");
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
 }
 
-function closeModal() {
-  editProfileModal.classList.remove("modal_is-opened");
-}
+profileEditButton.addEventListener("click", editProfile);
+editProfileModalCloseBtn.addEventListener("click", () =>
+  closeModal(editProfileModal)
+);
 
-function closeNewPostModal() {
-  newPostModal.classList.remove("modal_is-opened");
-}
-
-profileEditButton.addEventListener("click", openModal);
-editProfileModalCloseBtn.addEventListener("click", closeModal);
-
-newPostButton.addEventListener("click", openNewPostModal);
-newPostCloseBtn.addEventListener("click", closeNewPostModal);
+newPostButton.addEventListener("click", () => openModal(newPostModal));
+newPostCloseBtn.addEventListener("click", () => closeModal(newPostModal));
 
 function handleEditProfileSubmit(evt) {
   evt.preventDefault();
   profileNameEl.textContent = editProfileNameInput.value;
   profileDescriptionEl.textContent = editProfileDescriptionInput.value;
 
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 }
 
 // Create the form submission handler.
@@ -102,8 +87,8 @@ function handleAddCardSubmit(evt) {
 
   console.log(nameInput.value);
   console.log(linkInput.value);
-  closeNewPostModal();
+  closeModal(newPostModal);
 }
 
-editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+editProfileModal.addEventListener("submit", handleEditProfileSubmit);
 newPostModal.addEventListener("submit", handleAddCardSubmit);
