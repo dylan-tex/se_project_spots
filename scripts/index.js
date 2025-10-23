038e1bf7826ffb806ed4cf6eee549c8f9bc38746
Dylan Silver <dylansilver3@gmail.com>
2025-10-23 15:46:36 -0400

    Comments Added

--- scripts/index.js
+++ scripts/index.js
@@ -25,11 +25,11 @@ const initialCards = [
   },
 ];
 
-// Stores the name of the Profile that is pre-populated in the document upon start-up
+// Selects the profile name and description elements from the HTML file
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
-// Creates the elements of the Edit Profile  Modal (close button and two inputs ) and assigns them the corresponding values in the first object with the class edit-profile-modal in the HTML file
+// Creates the elements of the Edit Profile Modal (close button and two inputs ) and assigns them the corresponding values in the first object with the class edit-profile-modal in the HTML file
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileNameInput = editProfileModal.querySelector(
   "#profile-name-input"
@@ -41,98 +41,106 @@ const profileEditButton = document.querySelector(".profile__edit-btn");
 const editProfileModalCloseBtn =
   editProfileModal.querySelector(".modal__close-btn");
 
-// Creates the elements of the New Post Modal (close button and two inputs) and assigns them their respective values in the HTML file
-//newPostModal is a new modal
+// Creates the elements of the New Post Modal (close button and two inputs ) and assigns them the corresponding values in the first object with the class new-post-modal in the HTML file
 const newPostModal = document.querySelector("#new-post-modal");
 const captionInput = newPostModal.querySelector("#new-post-caption-input");
 const linkInput = newPostModal.querySelector("#new-post-link-input");
 const newPostButton = document.querySelector(".profile__add-btn");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+// Selects the card template from the HTML file.
 const cardTemplate = document
   .querySelector("#card-template")
   .content.querySelector(".card");
 
+// Selects the container where the cards will be added.
 const cardsList = document.querySelector(".cards__list");
 
+// Function to create a card element from the template and populate it with data.
 function getCardElement(data) {
-  //console.log(data);
+  // Clone the card template.
   const cardElement = cardTemplate.cloneNode(true);
   const cardTitleEl = cardElement.querySelector(".card__title");
   const cardImageEl = cardElement.querySelector(".card__image");
 
+  // Populate the card with data.
   cardImageEl.src = data.link;
   cardImageEl.alt = data.name;
   cardTitleEl.textContent = data.name;
+
   return cardElement;
 }
 
+// Function to open a given modal.
 function openModal(modal) {
+  // Add a class to indicate the modal is opened.
   modal.classList.add("modal_is-opened");
 }
 
+// Function to open the edit profile modal and pre-fill the input fields with the current profile information.
 function editProfile() {
+  // Open the edit profile modal.
   openModal(editProfileModal);
+
+  // Pre-fill the input fields with the current profile name and description.
   editProfileNameInput.value = profileNameEl.textContent;
   editProfileDescriptionInput.value = profileDescriptionEl.textContent;
 }
 
+// Function to close a given modal.
 function closeModal(modal) {
+  // Remove the class that indicates the modal is opened.
   modal.classList.remove("modal_is-opened");
 }
 
+// Event listeners for opening and closing the edit profile modal.
 profileEditButton.addEventListener("click", editProfile);
 editProfileModalCloseBtn.addEventListener("click", () =>
   closeModal(editProfileModal)
 );
 
+// Event listeners for opening and closing the new post modal.
 newPostButton.addEventListener("click", () => openModal(newPostModal));
 newPostCloseBtn.addEventListener("click", () => closeModal(newPostModal));
 
+// Function to handle the submission of the edit profile form.
 function handleEditProfileSubmit(evt) {
   evt.preventDefault();
+
+  // Update the profile name and description with the input values.
   profileNameEl.textContent = editProfileNameInput.value;
   profileDescriptionEl.textContent = editProfileDescriptionInput.value;
 
+  // Close the edit profile modal.
   closeModal(editProfileModal);
 }
 
-// Create the form submission handler.
+// Function to handle the submission of the new post form.
 function handleAddCardSubmit(evt) {
-  // Prevent default browser behavior.
+  // Prevent the default form submission behavior.
   evt.preventDefault();
 
-  /* this is useful to determine if the newPostModal is working properly*/
-  /*
-  console.log(nameInput.value);
-  console.log(linkInput.value);
-  */
-
-  //// Create a new card object from the input values.
-  //1:33 into Stage 8 Part 3 Video
+  // Gather the input values into an object.
   const inputValues = {
     name: captionInput.value,
     link: linkInput.value,
   };
 
-  //// Create a new card element using the getCardElement function.
+  // Create a card element using the getCardElement function.
   const cardElement = getCardElement(inputValues);
   cardsList.prepend(cardElement);
-  //////
 
   closeModal(newPostModal);
 }
 
-// Made these event listeners more clear
+// Attach the form submission handler to the edit profile form.
 editProfileModal.addEventListener("submit", handleEditProfileSubmit);
 newPostModal.addEventListener("submit", handleAddCardSubmit);
 
 initialCards.forEach(function (item) {
-  //console.log(item.name);
-  // console.log(item.link);
-  // this was creating a direct output but not creating an object
+  // Creates a Card element for each item in the initialCards array.
   const cardElement = getCardElement(item);
 
-  // may run into some issues with this per video walk through appending here vs prepending however prepending they end up in reverse order.
+  // Append the card element to the cards list.
   cardsList.append(cardElement);
 });
