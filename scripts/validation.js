17c0f56451fd0a66d3e2830763bc5d3892914c86
Dylan Silver <dylansilver3@gmail.com>
2025-12-31 23:29:36 -0400

    Validity State Working + modal__form class added to index.html

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,3 +1,35 @@
-const enableValidation = () => {
-  // Example validation function
+const checkInputValidity = (formEl, inputElement) => {
+  console.log(inputElement.validity);
 };
+
+const setEventListeners = (formEl) => {
+  console.log("Form element:", formEl);
+  const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
+  console.log("Found inputs:", inputList);
+  const buttonElement = formEl.querySelector(".modal__submit-btn");
+
+  console.log(inputList);
+  console.log(buttonElement);
+  // To - Do: Implement toggleButtonState function
+  //toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    console.log("target");
+    inputElement.addEventListener("input", function () {
+      console.log("input event");
+      checkInputValidity(formEl, inputElement);
+      //toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+// Form Validation Script
+const enableValidation = () => {
+  const formlist = document.querySelectorAll(".modal__form");
+
+  formlist.forEach((formEl) => {
+    setEventListeners(formEl);
+  });
+};
+
+enableValidation();
